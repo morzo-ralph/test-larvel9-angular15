4faca2f572560c743c7ccfaba698c55d713d2f68
morzo-ralph <201811037@gordoncollege.edu.ph>
2023-01-06 16:30:47 +0800

    larvel crud checked using postman

---
+++ api/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Article::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Article::create($request->all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Article::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Article::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if(Article::where('id', $id)->exists()){
+            $article = Article::find($id);
+            $article->title = $request->title;
+            $article->body = $request->body;
+            $article->author = $request->author;
+
+            $article->save();
+
+            return response()->json(["message" => "record updated successfully"], 200);
+        } else {
+            return response()->json(["message" => "article not found"], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(Article::where('id', $id)->exists()){
+            $article = Article::find($id);
+            $article->delete();
+
+            return response()->json(["message" => "record deleted"], 200);
+        } else {
+            return response()->json(["message" => "article not found"], 404);
+        }
+    }
+}
