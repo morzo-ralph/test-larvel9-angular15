88116facb9289ab423947cf680921d9b5a577bdd
morzo-ralph <201811037@gordoncollege.edu.ph>
2023-01-06 16:57:13 +0800

    add components

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { ObservableModule } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    UserModule,
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
